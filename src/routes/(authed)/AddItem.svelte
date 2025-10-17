7891ffc7fc2066cfd6a95f5e9d772918404b0e80
Nursultan Mambetzhanov <nmambetzhanov@innovationdigital.kg>
2025-10-17 17:46:27 +0600

    added component to add new categories

---
+++ src/routes/(authed)/AddItem.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	const { type, onClick } = $props();
+</script>
+
+<button
+    onclick={onClick}
+	class="relative cursor-pointer flex w-20 min-h-[86px] flex-col items-center justify-center rounded border border-dashed border-indigo-400 px-1.5 py-0.5"
+>
+	<span class="w-fit absolute top-0.5">{type}</span>
+	<span class="text-4xl">+</span>
+</button>
